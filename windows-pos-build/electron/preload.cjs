0d244ecb8128605b9e8ee915ae9c194b671e4368
4rp77v6vv9-alt <4rp77v6vv9@privaterelay.appleid.com>
2026-09-21 13:10:01 +0500

    Improve Windows POS desktop shell 20.31.48

--- windows-pos-build/electron/preload.cjs
+++ windows-pos-build/electron/preload.cjs
@@ -2,11 +2,16 @@ const {contextBridge,ipcRenderer}=require('electron')
 contextBridge.exposeInMainWorld('brooklynDesktop',{
   isDesktop:true,
   platform:process.platform,
-  version:'20.31.47',
+  version:'20.31.48',
   getConfig:()=>ipcRenderer.invoke('config:get'),
   saveConfig:value=>ipcRenderer.invoke('config:set',value),
   openHardwareSettings:()=>ipcRenderer.invoke('settings:open'),
+  openServerSettings:()=>ipcRenderer.invoke('server:settings'),
   toggleFullscreen:()=>ipcRenderer.invoke('window:toggleFullscreen'),
+  minimize:()=>ipcRenderer.invoke('window:minimize'),
+  toggleMaximize:()=>ipcRenderer.invoke('window:toggleMaximize'),
+  close:()=>ipcRenderer.invoke('window:close'),
+  reloadPos:()=>ipcRenderer.invoke('pos:reload'),
   hardwareStatus:()=>ipcRenderer.invoke('hardware:status'),
   hardwareCommand:(channel,payload)=>ipcRenderer.invoke('hardware:command',{channel,payload})
 })
