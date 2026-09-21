065f36970063896f68bdb483fe1ab0929ea86b08
4rp77v6vv9-alt <4rp77v6vv9@privaterelay.appleid.com>
2026-09-21 13:26:45 +0500

    Simplify Windows POS chrome and settings

--- windows-pos-build/electron/preload.cjs
+++ windows-pos-build/electron/preload.cjs
@@ -6,12 +6,8 @@ contextBridge.exposeInMainWorld('brooklynDesktop',{
   getConfig:()=>ipcRenderer.invoke('config:get'),
   saveConfig:value=>ipcRenderer.invoke('config:set',value),
   openHardwareSettings:()=>ipcRenderer.invoke('settings:open'),
-  openServerSettings:()=>ipcRenderer.invoke('server:settings'),
-  toggleFullscreen:()=>ipcRenderer.invoke('window:toggleFullscreen'),
   minimize:()=>ipcRenderer.invoke('window:minimize'),
-  toggleMaximize:()=>ipcRenderer.invoke('window:toggleMaximize'),
   close:()=>ipcRenderer.invoke('window:close'),
-  reloadPos:()=>ipcRenderer.invoke('pos:reload'),
   hardwareStatus:()=>ipcRenderer.invoke('hardware:status'),
   hardwareCommand:(channel,payload)=>ipcRenderer.invoke('hardware:command',{channel,payload})
 })
